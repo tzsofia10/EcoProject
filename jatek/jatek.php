614d1bd020d21be070373bf892d816dd5734beff
Zsófia Szofia <tzsofia07@gmail.com>
2024-12-10 08:52:57 +0100

    jatek

---
+++ jatek/jatek.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2D Platformer</title>
+    <link rel="stylesheet" href="../jatek/jatek.css">
+</head>
+<body>
+    <?php include '../html/navbar.php'; ?>
+    <canvas id="gameCanvas"   width= "1900px"
+    height=" 400px"></canvas>
+    <div id="pointsContainer"></div>
+    <script src="game.js"></script>
+</body>
+</html>
